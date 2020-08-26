13fd080c7a42abdf56fe4e479cb13fd58ce1fe9b
bshin95 <shinbryann@gmail.com>
2020-08-26 15:23:27 -0400

    button changes

--- project-setup/src/App.svelte
+++ project-setup/src/App.svelte
@@ -1,8 +1,17 @@
 <script>
   import Header from './UI/Header.svelte'
   import MeetupGrid from './Meetups/MeetupGrid.svelte'
+  import TextInput from './UI/TextInput.svelte'
+  import Button from "./UI/Button.svelte"
 
-  const meetups = [
+  let title = '';
+  let subtitle = '';
+  let address = '';
+  let email = '';
+  let description = '';
+  let imageUrl = '';
+
+  let meetups = [
     {
       id: 'm1',
       title: 'Coding Bootcamp',
@@ -22,16 +31,82 @@
       contactEmail: 'swimming@test.com'
     }
   ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      contactEmail: email,
+      address: address
+    };
+
+    // meetups.push(newMeetup); //this does not work
+    meetups = [newMeetup, ...meetups] //the = sign lets svelte recognize that it needs to be updated and then updates the DOM
+  }
 </script>
 
 <style>
   main {
     margin-top: 5rem;
   }
+
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
 </style>
 
 <Header />
 
 <main>
+  <form on:submit|preventDefault="{addMeetup}">
+    <TextInput 
+      id="title" 
+      label="Title" 
+      value={title} 
+      type="text"
+      on:input="{(event) => (title = event.target.value)}"
+    />
+    <TextInput 
+      id="subtitle" 
+      label="Subtitle" 
+      value={subtitle} 
+      type="text"
+      on:input="{(event) => (subtitle = event.target.value)}"
+    />
+    <TextInput 
+      id="address" 
+      label="Address" 
+      value={address}
+      type="text"
+      on:input="{(event) => (address = event.target.value)}"
+    />
+    <TextInput 
+      id="imageUrl" 
+      label="Image URL" 
+      value={imageUrl} 
+      type="text"
+      on:input="{(event) => (imageUrl = event.target.value)}"
+    />
+    <TextInput 
+      id="email" 
+      label="Email" 
+      value={email} 
+      type="email"
+      on:input="{(event) => (email = event.target.value)}"
+    />
+    <TextInput 
+      id="description" 
+      label="Description" 
+      controlType="textarea"
+      value={description} 
+      on:input="{(event) => (description = event.target.value)}"
+    />
+    <Button type="submit" caption="Save"/>
+  </form>
   <MeetupGrid {meetups}/>
 </main>
---
+++ project-setup/src/UI/TextInput.svelte
@@ -0,0 +1,63 @@
+<script>
+  export let controlType;
+  export let id;
+  export let label;
+  export let rows;
+  export let value;
+  export let type;
+</script>
+
+<!-- {#if controlType === 'textarea'}
+  <div class="form-control">
+    <label for="description">Description</label>
+    <textarea rows="3" id="description" bind:value={description}/>
+  </div>
+{:else}
+  <div class="form-control">
+    <label for="title">Title</label>
+    <input type="text" id="title" bind:value={title}/>
+  </div>
+{/if} -->
+
+<style>
+  input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: none;
+  border-bottom: 2px solid #ccc;
+  border-radius: 3px 3px 0 0;
+  background: white;
+  padding: 0.15rem 0.25rem;
+  transition: border-color 0.1s ease-out;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #e40763;
+  outline: none;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+  width: 100%;
+}
+
+.form-control {
+  padding: 0.5rem 0;
+  width: 100%;
+  margin: 0.25rem 0;
+}
+
+</style>
+
+<div class="form-control">
+  <label for="{id}">{label}</label>
+  {#if controlType === 'textarea'}
+    <textarea {rows} {id} {value} on:input/>
+  {:else}
+    <input type="{type}" {id} {value} on:input />
+  {/if}
+</div>
